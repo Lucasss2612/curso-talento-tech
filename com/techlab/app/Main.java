cf8312aa29c53c427a9fd2d6746223f248f84239
lucas <lucasss2612@gmail.com>
2025-10-25 17:47:36 -0400

    utima actualizacion

--- com/techlab/app/Main.java
+++ com/techlab/app/Main.java
@@ -14,14 +14,14 @@ public class Main {
             System.out.println("1. Agregar producto");
             System.out.println("2. Listar productos");
             System.out.println("3. Crear pedido");
-            System.out.println("4. Listar pedidos");
+            System.out.println("4. Listado pedidos");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             
             try {
                 opcion = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException e) {
-                System.out.println("⚠️ Ingrese un número válido.");
+                System.out.println("⚠️ Ingrese un número valido.");
                 opcion = -1;
             }
 
@@ -30,7 +30,7 @@ public class Main {
                 case 2 -> tienda.listarProductos();
                 case 3 -> tienda.crearPedido();
                 case 4 -> tienda.listarPedidos();
-                case 0 -> System.out.println("👋 Saliendo...");
+                case 0 -> System.out.println("👋 Saliendo.");
                 default -> System.out.println("Opción inválida.");
             }
         } while (opcion != 0);
